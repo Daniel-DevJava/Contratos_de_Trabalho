2538221452ed12cdd3ba0acf2347fee36a246814
Daniel Dias <danieldias.devjava@gmail.com>
2023-08-01 10:31:29 -0300

    Implementando o metodo removeContract

--- src/entities/Worker.java
+++ src/entities/Worker.java
@@ -60,8 +60,17 @@ public class Worker {
 		this.department = department;
 	}
 
+	public List<HourContract> getContracts() {
+		return contracts;
+	}
+
+	public void addContract(HourContract contract) {
+		contracts.add(contract);
+	}
 	
-	
+	public void removeContract(HourContract contract) {
+		contracts.remove(contract);
+	}
 	
 }
 
