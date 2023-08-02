83905876407afa0a4c0ba826c0383e1ab115edf9
Daniel Dias <danieldias.devjava@gmail.com>
2023-08-02 10:46:04 -0300

    Iniciando o desenvolvimento do sistema de Contratos

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,42 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Department;
+import entities.HourContract;
+import entities.Worker;
+import entities.enums.WorkerLevel;
+
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
-		System.out.println("Hello Word");
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		System.out.print("Enter department`s name:");
+		String departmentName = sc.nextLine();
+		System.out.print("Enter worker data:");
+		System.out.print("Name:");
+		String workerName = sc.nextLine();
+		System.out.print("Level:");
+		String workerLevel = sc.nextLine();
+		System.out.print("Base Salary");
+		double baseSalary = sc.nextDouble();
+		
+		Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary, new Department(departmentName));
+		
+		System.out.print("How many contracts to this worker?");
+	
+			
+		}
+		
 	}
 
-}
+
